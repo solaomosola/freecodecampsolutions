a90e758768b0620345e9b79e40fb0ece379adac6
solaomosola <olusolaomosola@yahoo.com>
2019-04-04 22:23:28 +0100

    Update Sum All Odd Fibonacci Numbers.js
    Intermediate Algorithm Scripting: Sum All Odd Fibonacci Numbers
    Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
    
    The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
    
    For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
    
    Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.

--- Sum All Odd Fibonacci Numbers.js
+++ Sum All Odd Fibonacci Numbers.js
@@ -1,23 +1,30 @@
+/*
+Intermediate Algorithm Scripting: Sum All Odd Fibonacci Numbers
+Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+*/
+
 function sumFibs(num) {
-  let sum = 0;
-  let first=1,second=1;
-  let count = 0;
-  let fib = 0;
-  while(sum<num){
-    console.log(`first = ${first}`)
-    console.log(`second = ${second}`)
-    sum = first + second;
-    fib += (first%2)===0?0:first ;
-    fib += (second%2)===0?0:second
-    //console.log(first,(first%2)===0?0:first,second,(second%2)===0?0:second, fib)
-    first = second;
-    second = sum;
-   
+  let fibArr = [1, 1];
+  let nextFib = 0;
+  let goAgain = true;
+  while (goAgain) {
+    nextFib = fibArr[fibArr.length - 2] + fibArr[fibArr.length - 1];
+    if (nextFib <= num) {
+      fibArr.push(nextFib)
+    }
+    else goAgain = false;
   }
+  let sum = fibArr.reduce((a, b) => {
+    return a + (b % 2 === 0 ? 0 : b);
+  })
   console.log(sum)
-   console.log(fib)
-  
   return sum;
 }
 
-sumFibs(10);
+sumFibs(4);
